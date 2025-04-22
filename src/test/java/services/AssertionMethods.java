61736edbbaf8d2455236c2b7eee6960cbc596390
Shailu08 <ssinghlodhi08@gmail.com>
2025-04-23 00:03:04 +0530

    ff

---
+++ src/test/java/services/AssertionMethods.java
@@ -0,0 +1,34 @@
+package services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AssertionMethods {
+
+//	Assertions -> validating actual result with expected result
+
+	// Assertions class
+
+	@Test
+	public void test1() {
+		System.out.println("Testing Some Assertion methods");
+		int actual = 12;
+		int expexted = 12;
+
+		// overloaded methods
+		Assertions.assertEquals(expexted, actual);
+
+		// Array
+		int[] actualIntArray = { 1, 2, 3, 4, 5, 6 };
+		int[] expectedIntArray = { 1, 2, 3, 4, 5, 6 };
+
+		Assertions.assertArrayEquals(expectedIntArray, actualIntArray);
+
+		// Assertions.assertSame
+		// Assertions.assertNull
+		// Assertions.assertNotNull
+		// Assertions.assertTrue
+
+	}
+
+}
--- src/test/java/services/CalculatorServiceTestJUnit5.java
+++ src/test/java/services/CalculatorServiceTestJUnit5.java
@@ -1,22 +1,64 @@
 package services;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
 
 public class CalculatorServiceTestJUnit5 {
-	
+
+//	execute the logic before all test cases
+	@BeforeAll
+	public static void init() {
+		System.out.println("call Before all Test cases");
+	}
+
+//	Execute After all Test Cases
+	@AfterAll
+	public static void cleanup() {
+		System.out.println("call After all Test cases");
+	}
+
+//	execute before each test case
+	@BeforeEach
+	public void beforeEachTestCase() {
+		System.out.println("call beforeEachTestCase");
+	}
+
+	// execute after each test case
+	@AfterEach
+	public void afterEachTestCase() {
+		System.out.println("call afterEachTestCase");
+	}
+
 	@Test
+	@DisplayName("This is custom name")
 	public void addTwoNumbersTest() {
-		
+		System.out.println("addTwoNumbersTest Test case");
 		int actualResult = CalculatorService.addTwoNumbers(12, 12);
 		int expectedResult = 24;
 		Assertions.assertEquals(expectedResult, actualResult);
-		
+
 	}
-	
-//	@Test
-//	public void addAnyNumbersTest() {
-//		
-//	}
+
+	@Test
+	@Disabled
+	public void addAnyNumbersTest() {
+		System.out.println("addAnyNumbersTest Test case");
+		int actualResult = CalculatorService.sumAnyNumbers(12, 12, 12, 12);
+		int expectedResult = 48;
+		Assertions.assertEquals(expectedResult, actualResult);
+	}
+
+//	@Tag
+//	@Nested
+//	@TestFactory
 
 }
